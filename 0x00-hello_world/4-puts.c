80054e3f95e443c35ab73ec05b12c859e573a950
jerry645 <ampahjeremiah17@gmail.com>
2022-09-08 17:19:01 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * main - main block
+ * main - 'main' block
  * retuern 0
  */
 int main(void)
 
 {
 
- puts("\"programming is like building a multilingual puzzle");
- return (0);
+puts("\"programming is like building a multilingual puzzle");
+return (0);
 
 }
