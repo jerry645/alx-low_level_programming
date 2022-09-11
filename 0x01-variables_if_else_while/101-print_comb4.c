9bb3e287f29db0e57f36989d67f019a1aaa270c2
jerry645 <ampahjeremiah17@gmail.com>
2022-09-11 10:42:04 -0700

    task11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,7 +21,7 @@ int main(void)
 					putchar(n);
 					putchar(m);
 					putchar(l);
-					if (n != 55 || m != 56 l != 57)
+					if (n != 55 || m != 56 || l != 57)
 					{
 						putchar(',');
 						putchar(' ');
