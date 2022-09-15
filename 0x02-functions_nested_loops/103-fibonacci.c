00185876c3f4db59a59aaf771961d80352aef848
jerry645 <ampahjeremiah17@gmail.com>
2022-09-14 19:07:42 -0700

    task15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - prints first 50 fibonacci
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	long int i, x = 1, y = 2, sum = 0, tsum = 0;
+
+	for (i = 0; i < 49; i++)
+
+	{
+		if ((y % 2 == 0) && ( y <= 4000000))
+		{
+			tsum = tsum + y;
+		}
+		sum = x + y;
+		x = y;
+		y = sum;
+	}
+	peintf("%ld\n", tsum);
+	return (0);
+}
+
