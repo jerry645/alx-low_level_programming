0496acb28f483a7453b9411aba6d27ed0fca544e
jerry645 <ampahjeremiah17@gmail.com>
2022-09-19 22:58:06 -0700

    task7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,7 +8,7 @@
 
 void puts_half(char *str)
 {
-	int len = 0, 1, n;
+	int len = 0, i, n;
 
 	while (str[len] != '\0')
 		len++;
@@ -16,5 +16,5 @@ void puts_half(char *str)
 		n = len / 2;
 	for (i = n; i < len; i++)
 		_putchar(str[i]);
-	-putchar('\n');
+	_putchar('\n');
 }
